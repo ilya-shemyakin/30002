0a22dfa8c68e60f347484b3ac86ae67c0bec643d
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-04-01 23:21:23 +0300

    Fixed output

--- romanchuk.kirill/T2/InputFormatters.cpp
+++ romanchuk.kirill/T2/InputFormatters.cpp
@@ -149,7 +149,7 @@ std::ostream& operator<<(std::ostream& out, const DataStruct& dest)
   out << "(:";
   out << "key1 " << std::fixed << std::setprecision(1) << dest.key1 << "d:";
   out << "key2 " << dest.key2 << ":";
-  out << "key3 " << dest.key3;
+  out << "key3 \"" << dest.key3 << "\"";
   out << ":)";
   return out;
 }

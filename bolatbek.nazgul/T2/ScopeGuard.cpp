6747c7d77f091c66b5e99be29f1c15664444e648
Bolatbek Nazgul <166728273+Nazkaaa@users.noreply.github.com>
2024-04-30 11:27:32 +0300

    ScopeGuard.cpp

---
+++ bolatbek.nazgul/T2/ScopeGuard.cpp
@@ -0,0 +1,18 @@
+#include "ScopeGuard.hpp"
+
+namespace bolatbek
+{
+  iofmtguard::iofmtguard(std::basic_ios< char > &s) noexcept:
+    s_(s),
+    fill_(s.fill()),
+    precision_(s.precision()),
+    fmt_(s.flags())
+  {}
+
+  iofmtguard::~iofmtguard()
+  {
+    s_.fill(fill_);
+    s_.precision(precision_);
+    s_.flags(fmt_);
+  }
+}

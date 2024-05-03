57069eff106594b127b414148a85237ea75011c0
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-05-03 04:32:45 +0300

    Add operators >>, << for Polygon

--- romanchuk.kirill/T3/Polygon.h
+++ romanchuk.kirill/T3/Polygon.h
@@ -10,5 +10,7 @@ struct Polygon
   std::vector< Point > points;
 };
 
+std::istream& operator>>(std::istream& in, Polygon& polygon);
+std::ostream& operator<<(std::ostream& out, const Polygon& polygon);
 
 #endif // !POLYGON_H

a67385968d92572984750cc41eafda6b71b54a9f
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-05-03 04:51:21 +0300

    Add readPolygonsFromFile function

---
+++ romanchuk.kirill/T3/ReadPolygonsFromFile.h
@@ -0,0 +1,34 @@
+#ifndef READPOLYGONSFROMFILE_H
+#define READPOLYGONSFROMFILE_H
+
+#include <fstream>
+
+#include "Polygon.h"
+
+std::vector< Polygon > readPolygonsFromFile(std::string pathToFile)
+{
+  std::ifstream file(pathToFile);
+  if (!file.is_open())
+  {
+    throw std::runtime_error("Error reading the file");
+  }
+  std::vector< Polygon > polygons;
+  while (!file.eof())
+  {
+    std::copy
+    (
+      std::istream_iterator< Polygon >{ file },
+      std::istream_iterator< Polygon >{},
+      std::back_inserter(polygons)
+    );
+    if (file.fail() && !file.eof())
+    {
+      file.clear();
+      file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  file.close();
+  return polygons;
+}
+
+#endif // !READPOLYGONSFROMFILE_H

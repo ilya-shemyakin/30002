678b37d3ce5f48fd29172b558ab62b14d46e4a90
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-05-03 03:52:32 +0300

    Add InputFormatters and >>, << operators for Point

--- romanchuk.kirill/T3/Point.cpp
+++ romanchuk.kirill/T3/Point.cpp
@@ -1,1 +1,30 @@
 #include "Point.h"
+
+#include "InputFormatters.h"
+
+std::istream& operator>>(std::istream& in, Point& point)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  Point tmp = {};
+  in >> DelimiterI{ '(' } >> tmp.x >> DelimiterI{ ';' } >> tmp.y >> DelimiterI{ ')' };
+  if (!in.fail())
+  {
+    point = tmp;
+  }
+  return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Point& point)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+  out << '(' << point.x << ';' << point.y << ')';
+  return out;
+}
--- romanchuk.kirill/T3/Point.h
+++ romanchuk.kirill/T3/Point.h
@@ -1,9 +1,14 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include <iostream>
+
 struct Point
 {
   int x, y;
 };
+
+std::istream& operator>>(std::istream& in, Point& point);
+std::ostream& operator<<(std::ostream& out, const Point& point);
 
 #endif // !POINT_H

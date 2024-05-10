78f32b3bd5369a30712552d1b5d2296368ff9e8a
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-05-11 00:40:11 +0300

    Bring std::out_of_range back

--- romanchuk.kirill/T3/main.cpp
+++ romanchuk.kirill/T3/main.cpp
@@ -41,6 +41,10 @@ int main(int argc, char** argv) {
       commands.at(argument)(std::cin, std::cout);
       std::cout << '\n';
     }
+    catch (const std::out_of_range&) {
+      std::cout << "<INVALID COMMAND>\n";
+      cleanIStream(std::cin);
+    }
     catch (const std::invalid_argument&) {
       std::cout << "<INVALID COMMAND>\n";
       cleanIStream(std::cin);

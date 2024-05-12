dcfa590d1a71c7fe73330f66b3d4736faee68058
bagrova-av <bagrova.av@edu.spbstu.ru>
2024-05-12 18:51:28 +0300

    Removed extra spaces

--- bagrova.anastasia/T3/StreamGuard.h
+++ bagrova.anastasia/T3/StreamGuard.h
@@ -6,20 +6,20 @@
 class StreamGuard
 {
 public:
-	StreamGuard(std::basic_ios<char>& stream) :
-		stream(stream),
-		precision(stream.precision()),
-		flags(stream.flags())
-	{}
-	~StreamGuard()
-	{
-		stream.precision(precision);
-		stream.flags(flags);
-	}
+    StreamGuard(std::basic_ios<char>& stream) :
+        stream(stream),
+        precision(stream.precision()),
+        flags(stream.flags())
+    {}
+    ~StreamGuard()
+    {
+        stream.precision(precision);
+        stream.flags(flags);
+    }
 
 private:
-	std::basic_ios<char>& stream;
-	std::streamsize precision;
-	std::basic_ios<char>::fmtflags flags;
+    std::basic_ios<char>& stream;
+    std::streamsize precision;
+    std::basic_ios<char>::fmtflags flags;
 };
 #endif

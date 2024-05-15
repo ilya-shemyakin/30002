4112c8953112707d107346485d39e5bd28724ac4
saveliyb-edu <bazhenovsp.edu@gmail.com>
2024-05-15 12:52:05 +0300

    Initial commit for T2

---
+++ bazhenov.saveliy/T2/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+
+#include "DataStruct.h"
+#include "InputFormatters.h"
+
+int main() {
+    std::vector< DataStruct > dataStructVector;
+    DataStruct ds;
+    while (!std::cin.eof()) {
+        std::copy(std::istream_iterator< DataStruct >(std::cin),
+                       std::istream_iterator< DataStruct >(),
+                       std::back_inserter(dataStructVector));
+
+        if (std::cin.fail() && !std::cin.eof()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+        }
+    }
+
+    std::sort(dataStructVector.begin(), dataStructVector.end());
+
+    std::copy(dataStructVector.begin(),
+        dataStructVector.end(),
+        std::ostream_iterator< DataStruct >(std::cout, "\n"));
+    return 0;
+}

47eed3485cfe1964e94a20838c0cddd28121bdcf
Bogdan Yuryev <yurev.bogdan.2005@mail.ru>
2024-05-30 11:42:14 +0300

    Initial commit for T3

---
+++ yurev.bogdan/T3/IOHelpers.cpp
@@ -0,0 +1,33 @@
+#include "IOHelpers.h"
+
+namespace yurev
+{
+    StreamGuard::StreamGuard(std::basic_ios<char> &s) : s_(s),
+                                                      fill_(s.fill()),
+                                                      precision_(s.precision()),
+                                                      fmt_(s.flags())
+    {}
+    StreamGuard::~StreamGuard()
+    {
+        s_.fill(fill_);
+        s_.precision(precision_);
+        s_.flags(fmt_);
+    }
+
+    std::istream &operator>>(std::istream &in, const ExpectedCharIO &&exp)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+        {
+            return in;
+        }
+
+        char c = in.get();
+        bool isExpected = exp.ignoreCase ? (std::tolower(c) == std::tolower(exp.val)) : (c == exp.val);
+        if (in && !isExpected)
+        {
+            in.setstate(std::ios::failbit);
+        }
+        return in;
+    }
+}

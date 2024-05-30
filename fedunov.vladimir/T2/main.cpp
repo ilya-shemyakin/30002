7aaae4f014a729cbfe0cb65ab10c07f29eff9d28
AZ09 <112526135+valdehooy@users.noreply.github.com>
2024-05-30 04:42:11 +0300

    Update main.cpp

--- fedunov.vladimir/T2/main.cpp
+++ fedunov.vladimir/T2/main.cpp
@@ -7,17 +7,26 @@
 int main()
 {
   std::vector<DataStruct> dataStructVector;
+  DataStruct ds;
 
+  // Используем std::copy для копирования данных из входного потока
   std::istream_iterator<DataStruct> in_begin(std::cin), in_end;
   std::copy(in_begin, in_end, std::back_inserter(dataStructVector));
 
   // Удаляем некорректные записи, вызвавшие failbit
-  dataStructVector.erase(std::remove_if(dataStructVector.begin(), dataStructVector.end(),
-    [](const DataStruct& ds) { return std::cin.fail(); }),
+  dataStructVector.erase(
+    std::remove_if(dataStructVector.begin(), dataStructVector.end(),
+      [](const DataStruct& ds) {
+        std::istringstream iss;
+        iss.str(ds.key3);
+        return iss.fail();
+      }),
     dataStructVector.end());
 
+  // Сортируем данные
   std::sort(dataStructVector.begin(), dataStructVector.end());
 
+  // Используем std::copy для вывода данных
   std::ostream_iterator<DataStruct> out_begin(std::cout, "\n");
   std::copy(dataStructVector.begin(), dataStructVector.end(), out_begin);
 

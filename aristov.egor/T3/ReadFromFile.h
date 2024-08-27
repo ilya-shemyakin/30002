060bf3a7f05deca252121f91aa8eb87842bda9ae
Egor_Ari100v <egorari100v@gmail.com>
2024-08-27 14:52:23 +0300

    Initial commit for T3
    Initial commit for T3

---
+++ aristov.egor/T3/ReadFromFile.h
@@ -0,0 +1,31 @@
+#ifndef READFROMFILE_H
+#define READFROMFILE_H
+
+#include<fstream>
+#include<iterator>
+#include"Polygon.h"
+
+void clean_istream(std::istream& in) {
+  in.clear();
+  in.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+}
+
+std::vector< Polygon > read_polygons_from_file(std::string path) {
+  std::ifstream file(path);
+  if (!file.is_open()) {
+    throw std::runtime_error("ERROR, failed attempt to read a file");
+  }
+  std::vector< Polygon > arr;
+  while (!file.eof()) {
+    std::copy(
+      std::istream_iterator< Polygon >{file}, std::istream_iterator< Polygon >{}, std::back_inserter(arr)
+    );
+    if (file.fail() && !file.eof()) {
+      clean_istream(file);
+    }
+  }
+  file.close();
+  arr.shrink_to_fit();
+  return arr;
+}
+#endif

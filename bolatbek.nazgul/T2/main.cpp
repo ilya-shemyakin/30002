748e57eeba20181e4104815bed38fabdfe3d9f66
Bolatbek Nazgul <166728273+Nazkaaa@users.noreply.github.com>
2024-09-07 19:36:40 +0300

    Create main.cpp

---
+++ bolatbek.nazgul/T2/main.cpp
@@ -0,0 +1,36 @@
+#include "structure.h"
+using DNLX::DataStruct;
+int main()
+{
+    try
+    {
+        std::string input = "";
+        std::vector< DataStruct > data;
+        while (!std::cin.eof())
+        {
+            std::copy(
+                std::istream_iterator< DataStruct >(std::cin),
+                std::istream_iterator< DataStruct >(),
+                back_inserter(data)
+            );
+            if (std::cin.fail() && !std::cin.eof())
+            {
+                std::cin.clear();
+            }
+        }
+        std::sort(std::begin(data), std::end(data), DNLX::compareDataStruct);
+        std::copy(
+            std::begin(data),
+            std::end(data),
+            std::ostream_iterator< DataStruct >(std::cout, "\n")
+        );
+    }
+    catch (std::exception& ex)
+    {
+        std::cerr << ex.what();
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+

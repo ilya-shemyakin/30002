5540c9b1cf14023620c2efb33e59dd414bc0ebcf
Bolatbek Nazgul <166728273+Nazkaaa@users.noreply.github.com>
2024-09-14 20:44:52 +0300

    Add files via upload

---
+++ bolatbek.nazgul/T2/libraries.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef LOCAL_H
+#define LOCAL_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <iomanip>
+#include <complex>
+#include <algorithm>
+#include <exception>
+#endif

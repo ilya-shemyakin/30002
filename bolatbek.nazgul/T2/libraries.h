0496a685007e7138ab16ed8d295883620d71f661
Bolatbek Nazgul <166728273+Nazkaaa@users.noreply.github.com>
2024-09-18 17:22:12 +0300

    commit for T2

--- bolatbek.nazgul/T2/libraries.h
+++ bolatbek.nazgul/T2/libraries.h
@@ -11,4 +11,3 @@
 #include <exception>
 #endif
 
-
--- bolatbek.nazgul/T2/main.cpp
+++ bolatbek.nazgul/T2/main.cpp
@@ -2,37 +2,33 @@
 using DNLX::DataStruct;
 int main()
 {
-    try
+  try
+  {
+    std::string input = "";
+    std::vector< DataStruct > data;
+    while (!std::cin.eof())
     {
-        std::string input = "";
-        std::vector< DataStruct > data;
-        while (!std::cin.eof())
-        {
-            std::copy(
-                std::istream_iterator< DataStruct >(std::cin),
-                std::istream_iterator< DataStruct >(),
-                back_inserter(data)
-            );
-            if (std::cin.fail() && !std::cin.eof())
-            {
-                std::cin.clear();
-            }
-        }
-        std::sort(std::begin(data), std::end(data), DNLX::compareDataStruct);
-        std::copy(
-            std::begin(data),
-            std::end(data),
-            std::ostream_iterator< DataStruct >(std::cout, "\n")
-        );
+      std::copy(
+        std::istream_iterator< DataStruct >(std::cin),
+        std::istream_iterator< DataStruct >(),
+        back_inserter(data)
+      );
+      if (std::cin.fail() && !std::cin.eof())
+      {
+        std::cin.clear();
+      }
     }
-    catch (std::exception& ex)
-    {
-        std::cerr << ex.what();
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+    std::sort(std::begin(data), std::end(data), DNLX::compareDataStruct);
+    std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
+    );
+  }
+  catch (std::exception& ex)
+  {
+    std::cerr << ex.what();
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
-
-
-
-

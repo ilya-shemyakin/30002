1545610823c532c2dba35e84616a88214d73f79f
MirryRow <ivanovigorek2004@gmail.com>
2024-09-19 21:38:14 +0300

    Remove old pred

--- ivanov.igor/T3/Predicates.cpp
+++ ivanov.igor/T3/Predicates.cpp
@@ -29,20 +29,4 @@ namespace ivanov
         return lhs.points.size() < rhs.points.size();
     }
 
-    bool hasIntersection(const Polygon& lhs, const Polygon& rhs)
-    {
-        const auto minMaxLhs = std::minmax_element(lhs.points.cbegin(), lhs.points.cend());
-        const auto minMaxRhs = std::minmax_element(rhs.points.cbegin(), rhs.points.cend());
-        const auto& maxLhs = *(minMaxLhs.second);
-        const auto& minLhs = *(minMaxLhs.first);
-        const auto& maxRhs = *(minMaxRhs.second);
-        const auto& minRhs = *(minMaxRhs.first);
-
-        return !((maxRhs < minLhs) || (maxLhs < minRhs));
-    }
-
-    bool isRect(const Polygon& p)
-    {
-        return p.points.size() == 4;
-    }
 }
